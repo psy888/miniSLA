c90d046f1eb5e2621ce69fec9914a82815f07ba2
psy888 <lpsyl888l@yandex.ru>
2020-11-06 19:05:17 +0200

    add custom validators to edit form

---
+++ src/app/shared/validators/custom.validators.ts
@@ -0,0 +1,17 @@
+import { FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { Series } from 'src/app/shared/services/device.service';
+
+export const seriesValidation: ValidatorFn = (control: FormControl): ValidationErrors | null => {
+  if (Object.keys(Series).includes(control.value)) {
+    return null;
+  }
+  return { error: true };
+};
+
+export const ramValidation: ValidatorFn = (control: FormControl): ValidationErrors | null => {
+  if (control.value % 8 === 0) {
+    return null;
+  }
+  return { error: true };
+
+};
