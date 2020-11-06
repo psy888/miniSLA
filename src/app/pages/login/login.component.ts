842c61aff6f79be24dc5cf19e3defb1a878d79ff
psy888 <lpsyl888l@yandex.ru>
2020-11-06 19:08:12 +0200

    refactor login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { LoadingStateService } from 'src/app/shared/services/loading-state.service';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +14,9 @@ export class LoginComponent implements OnInit {
   authForm: FormGroup;
   isNewUser = false;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              private router: Router,
+              private loadingService: LoadingStateService) {
   }
 
   ngOnInit(): void {
@@ -23,10 +27,32 @@ export class LoginComponent implements OnInit {
   }
 
   submit(): void {
+    this.loadingService.startLoadig();
     this.authService.login(
       this.authForm.value.email,
       this.authForm.value.password,
+    ).subscribe(response => {
+        if (!response.error) {
+          localStorage.setItem(this.authService.tokenKey, response.token);
+          this.toListPage();
+        } else {
+          this.updateForm(response.error);
+        }
+        this.loadingService.endLoading();
+      },
     );
   }
+
+  updateForm(error: string): void {
+    if (error) {
+      this.authForm.controls[error].setErrors({ 'server_error': true });
+      this.authForm.controls[error].markAsTouched();
+    }
+  }
+
+  toListPage(): void {
+    this.router.navigate(['/list']);
+  }
+
 
 }
