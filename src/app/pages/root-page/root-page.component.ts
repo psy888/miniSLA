ce657f591e93fc139001cf2804dda9d3120b9ffc
psy888 <lpsyl888l@yandex.ru>
2020-11-06 19:10:06 +0200

    fix is loading flag issue

--- src/app/pages/root-page/root-page.component.ts
+++ src/app/pages/root-page/root-page.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
+import { delay } from 'rxjs/operators';
+import { LoadingStateService } from 'src/app/shared/services/loading-state.service';
 
 @Component({
   selector: 'app-root-page',
   templateUrl: './root-page.component.html',
-  styleUrls: ['./root-page.component.scss']
+  styleUrls: ['./root-page.component.scss'],
 })
-export class RootPageComponent implements OnInit {
+export class RootPageComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('drawer') mainMenu: MatSidenav;
+
+  loadingData = false;
+
+  constructor(private router: Router,
+              private loadingService: LoadingStateService) {}
 
   ngOnInit(): void {
+    this.router.events.subscribe(event => this.closeMenuDrawer());
   }
+
+  ngAfterViewInit(): void {
+    this.loadingService.curState.pipe(
+      delay(0), // fix problem with ngIf
+    ).subscribe(value => this.loadingData = value);
+  }
+
+
+  closeMenuDrawer(): void {
+    this.mainMenu.close();
+  }
+
 
 }
